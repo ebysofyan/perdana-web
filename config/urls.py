a578fe6258a746b92f9198e71302933915fb0327
Eby Sofyan Fadly <eby.sofyan@gmail.com>
2019-07-22 21:36:45 +0800

    fix : fix dashboard and login page

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.member.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', LoginView.as_view(), name='login'),
     path('dashboard/', include(('apps.dashboard.urls', 'dashboard'), namespace='dashboard')),
 ]
 
